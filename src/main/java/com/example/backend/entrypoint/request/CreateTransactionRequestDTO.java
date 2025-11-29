c5245b40440e425a8fbfbe9b806749de95abacc2
pedrosferreira <ferreirapedro.sjm@gmail.com>
2025-11-29 18:54:27 +0000

    add constructor annotations

--- src/main/java/com/example/backend/entrypoint/request/CreateTransactionRequestDTO.java
+++ src/main/java/com/example/backend/entrypoint/request/CreateTransactionRequestDTO.java
@@ -2,11 +2,15 @@ package com.example.backend.entrypoint.request;
 
 import com.example.backend.core.domain.transaction.TransactionType;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateTransactionRequestDTO {
 
     private TransactionType transactionType;
